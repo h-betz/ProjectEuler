4ab967e911578f758a0111cf790e9cadbaf3510e
Hunter Betz <hhb311@gmail.com>
2015-08-27 22:29:01 -0400

    Update euler10.c

--- euler10.c
+++ euler10.c
@@ -2,77 +2,62 @@
 //  euler10.c
 //
 //  Problem 10 of Project Euler - Sum of all primes
-//  below two million
+//  below two million using Sieve of Eratosthenes
 //
 //  Created by Hunter Betz on 8/9/15.
 //
 //
 
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <math.h>
-
-bool isPrime(long int);
 
 
 int main() {
     
-    long int sum = 2 + 3;
-    long int prime = 0;
-    long int k = 1;
+    long int i;
     
-    //while prime is less than two mil, keep adding the sums of the primes
-    while (prime < 2000000) {
+    long int j;
+    long int n = 2000000;
+    int *prime;
+    prime = malloc(2000000 * sizeof(int));
+    
+    printf("Here\n");
+    
+    //assign all locations in array a value of 1
+    for (i = 0; i < n; i++) {
         
-        //check the lower bound
-        prime = (6 * k) - 1;
-        if (isPrime(prime) && prime < 2000000) {
-            sum += prime;
-        }
-        
-        //check the upper bound
-        prime = (6 * k) + 1;
-        if (isPrime(prime) && prime < 2000000) {
-            sum += prime;
-        }
-
-        k++;
+        prime[i] = 1;
         
     }
     
+    //sieve the non-primes from the array
+    for (i = 2; i < n; i++) {
+        
+        if (prime[i]) {
+            
+            for(j = i; i * j< n; j++) {
+                
+                prime[i * j] = 0;
+                
+            }
+            
+        }
+        
+    }
+
+    long int sum = 0;
+    //sum up the primes
+    for (i = 2; i < n; i++) {
+        if(prime[i]) {
+            sum += i;
+        }
+        
+    }
+
+    free(prime);
     printf("Sum: %ld\n", sum);
     
-    
-    return 0;
-}
 
-//Check to see if the number passed is a prime
-bool isPrime(long int d) {
-    
-    long int count = 1;
-    double square = sqrt(d);
-
-    //if the number is divisible by 2 or 3, its not a prime
-    if (d % 2 == 0 || d % 3 == 0) {
-        return false;
-    }
-    
-    long int prime = 3;
-    
-    while(prime <= square) {
-        
-        prime = (6 * count) - 1;
-        if (d % prime == 0) {
-            return false;
-        }
-        prime = (6 * count) + 1;
-        if (d % prime == 0) {
-            return false;
-        }
-        count++;
-    }
-    
-    //number is a prime then
-    return true;
     
 }
